bf401495384bd3de049b56ebe54e280a589ad873
Saxashitter <saxashitter@youthought.lol>
2026-08-09 02:34:47 -0400

    Menu polish

--- src/Lua/3_Extras/1_Menu/1_Setup/4_HUD.lua
+++ src/Lua/3_Extras/1_Menu/1_Setup/4_HUD.lua
@@ -1,5 +1,13 @@
-addHook("HUD", function(v)
-	if not PoyoPennynickel.Menu.Active then return end
+local MENU_TWEEN = 0
+local MENU_TICS = 8
+
+local function menu(v)
+	if not PoyoPennynickel.Menu.Active then
+		MENU_TWEEN = max(0, $ - FU / MENU_TICS)
+	else
+		MENU_TWEEN = min(FU, $ + FU / MENU_TICS)
+	end
+	if MENU_TWEEN == 0 then return end
 
 	local player = consoleplayer
 
@@ -10,16 +18,20 @@ addHook("HUD", function(v)
 	local scaleX = FixedDiv(screenWidth, background.width)
 	local scaleY = FixedDiv(screenHeight, background.height)
 
-	v.drawStretched(0, 0, scaleX, scaleY, background, V_SNAPTOLEFT|V_SNAPTOTOP|V_40TRANS)
+	local trans = 10 - FixedMul(10, MENU_TWEEN)
+	if trans < 10 then
+		v.drawStretched(0, 0, scaleX, scaleY, background, V_SNAPTOLEFT|V_SNAPTOTOP|(max(trans, 4) * V_10TRANS))
+	end
 
 	-- draw background
 	local banner = v.cachePatch("LTZIGRED")
 	local tics = 180
 	local t = FixedDiv(leveltime % tics, tics)
 	local offsetY = FixedMul(banner.height*FU, t)
+	local bannerX = ease.linear(MENU_TWEEN, -banner.width*FU, 0)
 
 	for i = -banner.height*FU, screenHeight*FU, banner.height*FU do
-		v.drawScaled(0, i + offsetY, FU, banner, V_SNAPTOLEFT|V_SNAPTOTOP)
+		v.drawScaled(bannerX, i + offsetY, FU, banner, V_SNAPTOLEFT|V_SNAPTOTOP)
 	end
 
 	-- bing chillin
@@ -36,10 +48,14 @@ addHook("HUD", function(v)
 		end
 	end
 
-	v.drawScaled(220*FU, 70*FU, FU/8, poyo, V_SNAPTORIGHT|V_SNAPTOBOTTOM, poyomap)
-	v.drawScaled(220*FU, 70*FU, FU/8, visor, V_SNAPTORIGHT|V_SNAPTOBOTTOM, translatemap)
+	local poyo_x = ease.linear(MENU_TWEEN, 320*FU, 220*FU)
+
+	v.drawScaled(poyo_x, 70*FU, FU/8, poyo, V_SNAPTORIGHT|V_SNAPTOBOTTOM, poyomap)
+	v.drawScaled(poyo_x, 70*FU, FU/8, visor, V_SNAPTORIGHT|V_SNAPTOBOTTOM, translatemap)
 
 	-- draw all entries
+	if trans == 10 then return end
+
 	local entries = PoyoPennynickel.Menu.Current.Entries
 	local stringHeight = 8
 	local y = 100 - (stringHeight * #entries / 2)
@@ -58,12 +74,12 @@ addHook("HUD", function(v)
 
 			option = string.char(28).." "..$.." "..string.char(29)
 
-			v.drawString(160 - sep, y + stringHeight * (i-1), entry.Name, V_ALLOWLOWERCASE|color, "thin-right")
-			v.drawString(160 + sep, y + stringHeight * (i-1), option, V_ALLOWLOWERCASE|color, "thin")
+			v.drawString(160 - sep, y + stringHeight * (i-1), entry.Name, V_ALLOWLOWERCASE|(trans * V_10TRANS)|color, "thin-right")
+			v.drawString(160 + sep, y + stringHeight * (i-1), option, V_ALLOWLOWERCASE|(trans * V_10TRANS)|color, "thin")
 			continue
 		end
 
-		v.drawString(160, y + stringHeight * (i-1), entry.Name, V_ALLOWLOWERCASE|color, "thin-center")
+		v.drawString(160, y + stringHeight * (i-1), entry.Name, V_ALLOWLOWERCASE|(trans * V_10TRANS)|color, "thin-center")
 	end
 
 	-- draw description of entry
@@ -77,8 +93,10 @@ addHook("HUD", function(v)
 		y = $ - 8
 	end
 
-	v.drawString(160, y, entry.Desc, V_SNAPTOBOTTOM|V_ALLOWLOWERCASE|V_REDMAP, "thin-center")
+	v.drawString(160, y, entry.Desc, V_SNAPTOBOTTOM|V_ALLOWLOWERCASE|V_REDMAP|(trans * V_10TRANS), "thin-center")
 	if entry.Options and entry.Options[entry.CurrentOption].desc then
-		v.drawString(160, y + 8, entry.Options[entry.CurrentOption].desc, V_SNAPTOBOTTOM|V_ALLOWLOWERCASE|V_REDMAP, "thin-center")
+		v.drawString(160, y + 8, entry.Options[entry.CurrentOption].desc, V_SNAPTOBOTTOM|V_ALLOWLOWERCASE|V_REDMAP|(trans * V_10TRANS), "thin-center")
 	end
-end)
+end
+
+addHook("HUD", menu)
